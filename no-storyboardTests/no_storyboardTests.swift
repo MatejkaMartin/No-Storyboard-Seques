5aa07d6c05148b83f29c0cbf6c08a6c8a65de8cd
Martin Prace <martinmatejka@martin-macbook-pro.local>
2016-07-25 09:38:13 +0200

    Initial Commit

---
+++ no-storyboardTests/no_storyboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  no_storyboardTests.swift
+//  no-storyboardTests
+//
+//  Created by Martin Matějka on 25.07.16.
+//  Copyright © 2016 Martin Matějka. All rights reserved.
+//
+
+import XCTest
+@testable import no_storyboard
+
+class no_storyboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
